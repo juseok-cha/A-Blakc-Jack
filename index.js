40e963673041c045bc67bfcadd4f091d8c5f1d00
doma_cha <62501120+money17@users.noreply.github.com>
2021-08-21 16:05:16 +0900

    Lesson 7
    Add the hasBlackJack variable.

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 let firstCard = 10
-let secondCard = 7 
+let secondCard = 11
 let sum = firstCard + secondCard
-
-// Write teh conditional according to these rules: 
-
-// if less than or equal to 20 -> " Do you want to draw a new card?"
-// else if exactly 21 -> "Wohoo! You've got Blackjack!"
-// else -> "You are out of the game"
+let hasBlackJack =  false
 
 if (sum <= 20 ) {
     console.log("Do you want to draw a new card?")
 } else if (sum === 21) {
     console.log("Wohoo! You've got Blackjack!")
+    hasBlackJack = true
 } else {
     console.log("You are out of the game.")
 }
+
+// CASH OUT!
+console.log(hasBlackJack)
