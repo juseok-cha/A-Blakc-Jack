20827d5403185f1bb1b8a7265961a539c04b95b7
doma_cha <62501120+money17@users.noreply.github.com>
2021-08-21 17:28:51 +0900

    Lesson 13
    Make the start button work.

--- index.js
+++ index.js
@@ -3,19 +3,24 @@ let secondCard = 11
 let sum = firstCard + secondCard
 let hasBlackJack =  false
 let isAlived = true
-// 1. Declare a variable called message and assign its value to an empty string. 
 let message = ""
 
-// 2. Reassign the message variable to the string we're logging out. 
-if (sum <= 20 ) {
-    message = "Do you want to draw a new card?"
-} else if (sum === 21) {
-    message = "Wohoo! You've got Blackjack!"
-    hasBlackJack = true
-} else {
-    massage = "You are out of the game."
-    isAlived = false
-}
 
-// 3. Log it out. 
-console.log(message)
+
+// 2. Create a startbutton. 
+function startGame(){
+
+    if (sum <= 20 ) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "Wohoo! You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        massage = "You are out of the game."
+        isAlived = false
+    }
+    
+    // 3. Log it out. 
+    console.log(message)
+
+}
