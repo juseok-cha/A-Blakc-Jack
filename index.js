2ef05981e333d3d6b8213d8e07681d8e8f898d5e
doma_cha <62501120+money17@users.noreply.github.com>
2021-08-21 16:00:01 +0900

    Lesson 6
    The if.. else statement for our game

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-// Check if the person is elegible for a birthday card from the King!
+let firstCard = 10
+let secondCard = 7 
+let sum = firstCard + secondCard
 
-let age = 100
+// Write teh conditional according to these rules: 
 
-// If less than 100            -> "Not elegible"
-// else if exactly 100         -> "Here is your birthday card from the King!"
-// else                        -> "Not elegible, you have already one"
+// if less than or equal to 20 -> " Do you want to draw a new card?"
+// else if exactly 21 -> "Wohoo! You've got Blackjack!"
+// else -> "You are out of the game"
 
-if (age < 100) {
-    console.log("Not elegible")
-} else if (age === 100) {
-    console.log("Here is your birthday card from the King!")
+if (sum <= 20 ) {
+    console.log("Do you want to draw a new card?")
+} else if (sum === 21) {
+    console.log("Wohoo! You've got Blackjack!")
 } else {
-    console.log("NOt elegible, you have already one")
+    console.log("You are out of the game.")
 }
