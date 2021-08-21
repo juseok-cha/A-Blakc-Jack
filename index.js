5857036388e82daf9b103619d02fdc19a90fd667
doma_cha <62501120+money17@users.noreply.github.com>
2021-08-21 16:08:36 +0900

    Lesson 8
     Add the isAlive variable

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ let firstCard = 10
 let secondCard = 11
 let sum = firstCard + secondCard
 let hasBlackJack =  false
+// 1. Create a variable called isAlived and assign it to true. 
+let isAlived = true
+
+// 2. Flip its value to false in the appropriate code block. 
 
 if (sum <= 20 ) {
     console.log("Do you want to draw a new card?")
@@ -10,6 +14,7 @@ if (sum <= 20 ) {
     hasBlackJack = true
 } else {
     console.log("You are out of the game.")
+    isAlived = false
 }
 
 // CASH OUT!
