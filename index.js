14ce624b9aa0280f6a91ec63091bb2ae372d5b26
doma_cha <62501120+money17@users.noreply.github.com>
2021-08-22 23:56:25 +0900

    Lesson 16 & 17
    Display the card
    New card button

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ let hasBlackJack =  false
 let isAlived = true
 let message = ""
 let messageEl = document.getElementById("message-el")
-// 2. Store the sum paragraph in a variable called sumEl.
 let sumEl = document.getElementById("sum-el")
-// let sumEl = document.querySelector("#sum-el")
-console.log(sumEl)
-
+// 2. Store teh cards paragraph in a variable called cardEl
+let cardsEl = document.querySelector("#cards-el")
+console.log(cardsEl)
 
 function startGame(){
-    // 3. Render the sum on the page using this format -> "Sum: 14"
+    // 3. Render teh cars on the page using this format -> "Cards 10 4"
+    cardsEl.textContent  = "Cards: " + firstCard + " "+secondCard
     sumEl.textContent = "Sum: " + sum
     if (sum <= 20 ) {
         message = "Do you want to draw a new card?"
@@ -26,4 +26,9 @@ function startGame(){
     
     messageEl.textContent = message
 
+}
+
+// 2. Create a function newCard() that logs out "Drawing a new card"
+function newCard() {
+    console.log("Drawing a new card")
 }
